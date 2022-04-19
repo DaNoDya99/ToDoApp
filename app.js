251cf3d142784e5fd11d57e0f12c67cca52bc363
DaNoDya99 <2020cs014@stu.ucsc.cmb.ac.lk>
2022-04-19 15:38:37 +0530

    Current location implemented

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const opencage = require('opencage-api-client');
+const { json } = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -11,6 +13,9 @@ app.use(express.static("public"));
 const tasks = [];
 const workList = [];
 const day = date.getDate();
+let locationAddress;
+let location;
+
 
 app.get("/", function (req, res) {
   res.render("index", { dayTitle: day, tasks: tasks });
@@ -36,7 +41,29 @@ app.get("/about",function(req,res){
   res.render("about");
 });
 
+app.get("/weather",function(req,res){
+  res.render("weather");
+});
+
+app.post("/weather",function(req,res){
+  const longi = req.body.longitude;
+  const lati = req.body.latitude;
+
+  opencage.geocode({q: lati+','+longi, key:'46659b54d9f944a5a7f35b490671677b'}).then((data)=> {
+    locationAddress = data.results[0].formatted;
+    location = data.results[0].components.city;
+
+    console.log(locationAddress+" "+location);
+  });
+});
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
+
+
+
+
+// Api Key
+// AIzaSyDyjhb8ZhlY7ZXGSKfzg6vbDF9hUxX3GzA
